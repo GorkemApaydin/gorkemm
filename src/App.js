6711e94eac7ae1976c2c75a0cba2a67031b91f37
GorkemApaydin <gorkem40@yandex.com>
2021-12-01 15:40:04 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './npmLogo.png';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Görkem Apaydın
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjsss.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go <b>ReactJS</b>
         </a>
       </header>
     </div>
